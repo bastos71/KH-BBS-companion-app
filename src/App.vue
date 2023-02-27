111e2c959ab8c06a1149d85e522ee66e02a7fe66
Bastien Serprix <bastien.serprix@gmail.com>
2023-02-27 22:05:31 +0100

    feat: add credits

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import GameLogo from './assets/bbsfm-logo.png'
     <img class="header__logo" :src="GameLogo" alt="" />
   </header>
 
-  <div class="container">
+  <section class="container">
     <ul class="nav justify-content-center">
       <li class="nav-item">
         <RouterLink class="nav-link kh-button" active-class="kh-button__active" to="/"
@@ -26,7 +26,24 @@ import GameLogo from './assets/bbsfm-logo.png'
     </ul>
 
     <RouterView />
-  </div>
+  </section>
+
+  <footer class="footer">
+    <div class="footer__inner">
+      <p class="m-0">
+        Crédits :
+        <i
+          >sources et données
+          <a href="https://www.khdestiny.fr/" target="_blank">Khdestiny.fr</a></i
+        >
+        |
+        <i
+          >buttons design by
+          <a href="https://codepen.io/erzulu/pen/gdqyGE" target="_blank">Erin Hughes</a></i
+        >
+      </p>
+    </div>
+  </footer>
 </template>
 
 <style scoped lang="scss">
@@ -48,5 +65,20 @@ import GameLogo from './assets/bbsfm-logo.png'
 
 .nav-item {
   margin: 1em;
+}
+
+.footer {
+  margin-top: auto;
+
+  &__inner {
+    background: var(--bs-dark-bg-subtle);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 1em;
+    font-size: 0.75em;
+    text-align: center;
+    margin-top: 2em;
+  }
 }
 </style>
